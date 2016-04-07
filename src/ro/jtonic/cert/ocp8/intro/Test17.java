3bebe0e2b6324256d1dd86a1213069fcb5e28a4d
jtonic <antonel.pazargic@gmail.com>
2016-04-07 11:16:36 +0300

    Some changes to some tests after the reading of the assessment tests result.

--- src/ro/jtonic/cert/ocp8/intro/Test17.java
+++ src/ro/jtonic/cert/ocp8/intro/Test17.java
@@ -3,6 +3,7 @@ package ro.jtonic.cert.ocp8.intro;
 import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,16 +13,19 @@ import java.io.IOException;
 /**
  * Created by antonelpazargic on 06/04/16.
  */
-public class Test15 {
+public class Test17 {
 
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("/gorilla/signed-words.txt");
-        Files.find(path.getParent(), 10.0, (Path p) -> p.toString().endsWith(".txt") && Files.isDirectory(p))
+        Files.find(path.getParent(), 10, //k1
+                (Path p, BasicFileAttributes attr) -> p.toString().endsWith(".txt") && Files.isDirectory(p)) // k2
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
-        Files.readAllLines(path)
-                .flatMap(p -> Stream.of(p.split(" ")))
-                .map(s -> s.toLowerCase())
+
+        Files.readAllLines(path) // k3
+//                .stream()
+                .flatMap(p -> Stream.of(p.split(" "))) // k4
+                .map(s -> s.toLowerCase()) // k5
                 .forEach(System.out::println);
 
     }
--- src/ro/jtonic/cert/ocp8/intro/Test7.java
+++ src/ro/jtonic/cert/ocp8/intro/Test7.java
@@ -8,8 +8,8 @@ import java.nio.file.*;
  */
 public class Test7 {
 
-    public static void main(String[] args) {
-        Path path1 = Paths.get("/bats/night", "../").resolve("./sleep.txt").normalize();
+    public static void main(String[] args) throws IOException {
+        Path path1 = Paths.get("/Users/antonelpazargic/jtonic/tmp/night", "../").resolve("./sort.swift").normalize();
         Path path2 = new File("../sleep.txt").toPath().toRealPath();
         System.out.println(Files.isSameFile(path1, path2));
         System.out.println(" " + path1.equals(path2));
