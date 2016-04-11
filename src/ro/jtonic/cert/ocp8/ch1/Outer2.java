e0fdbe3e23bb422cf57b267072af18df53afe99a
jtonic <antonel.pazargic@gmail.com>
2016-04-11 12:46:08 +0300

    example of local inner class which accesses an inner field of the outer class and a final local variable.

---
+++ src/ro/jtonic/cert/ocp8/ch1/Outer2.java
@@ -0,0 +1,25 @@
+package ro.jtonic.cert.ocp8.ch1;
+
+/**
+ * Created by antonelpazargic on 11/04/16.
+ */
+public class Outer2 {
+    private int length = 5;
+
+    public void calculate() {
+        final int width = 20;
+        class Inner {
+            public void multiply() {
+                System.out.println(length * width);
+            }
+        }
+        Inner inner = new Inner();
+        inner.multiply();
+    }
+
+    public static void main(String... args) {
+        Outer2 outer = new Outer2();
+        outer.calculate();
+    }
+
+}
