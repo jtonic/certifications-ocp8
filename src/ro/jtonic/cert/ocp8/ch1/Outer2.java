b88d35d60bc455a262d90313cb8e8e23fed46b81
jtonic <antonel.pazargic@gmail.com>
2016-04-11 12:47:39 +0300

    Local inner class which accesses a effective final local variable

--- src/ro/jtonic/cert/ocp8/ch1/Outer2.java
+++ src/ro/jtonic/cert/ocp8/ch1/Outer2.java
@@ -7,7 +7,7 @@ public class Outer2 {
     private int length = 5;
 
     public void calculate() {
-        final int width = 20;
+        int width = 20;
         class Inner {
             public void multiply() {
                 System.out.println(length * width);
