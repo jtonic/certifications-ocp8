dcfb39822158f5f01d4f7229e5653f013c673cb7
jtonic <antonel.pazargic@gmail.com>
2016-04-11 12:55:14 +0300

    Anonymous local inner class - example

---
+++ src/ro/jtonic/cert/ocp8/ch1/AnonInner.java
@@ -0,0 +1,27 @@
+package ro.jtonic.cert.ocp8.ch1;
+
+/**
+ * Created by antonelpazargic on 11/04/16.
+ */
+public class AnonInner {
+
+    abstract class SaleTodayOnly {
+        abstract int dollarsOff();
+    }
+
+    public int admission(int basePrice) {
+        SaleTodayOnly sale = new SaleTodayOnly() {
+            int dollarsOff() {
+                return 3;
+            }
+        };
+        return basePrice - sale.dollarsOff();
+    }
+
+    public static void main(String... args) {
+        AnonInner inner = new AnonInner();
+        final int admission = inner.admission(10);
+        System.out.println("admission = " + admission);
+    }
+
+}
