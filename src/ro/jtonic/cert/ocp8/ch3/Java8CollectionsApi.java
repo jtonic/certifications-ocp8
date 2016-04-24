45ad2d1eeba5c0dd5b7fda6d873f32aa00671882
jtonic <antonel.pazargic@gmail.com>
2016-04-24 18:01:04 +0300

    some corner cases for Map merge api.

--- src/ro/jtonic/cert/ocp8/ch3/Java8CollectionsApi.java
+++ src/ro/jtonic/cert/ocp8/ch3/Java8CollectionsApi.java
@@ -17,16 +17,29 @@ public class Java8CollectionsApi {
         favorites.putIfAbsent("Jenny", "Tram");
         favorites.putIfAbsent("Sam", "Tram");
         favorites.putIfAbsent("Tom", "Tram");
-        System.out.println(favorites);
+        System.out.println("After putIfAbsent: " + favorites);
 
 //        ===================
         favorites.clear();
         favorites.put("Jenny", "Bus Tour");
         favorites.put("Tom", "Tram");
+        favorites.put("Sam", null);
 
         BiFunction<String, String, String> mapper = (v1, v2) -> v1.length() > v2.length() ? v1 : v2;
         System.out.println(favorites.merge("Jenny", "Skyride", mapper));
         System.out.println(favorites.merge("Tom", "Skyride", mapper));
-        System.out.println(favorites);
+        System.out.println(favorites.merge("Sam", "Skyride", mapper));
+        favorites.merge("Dale", "Skyride", mapper);
+        System.out.println("After merge: " + favorites);
+
+        // merge example with a BiFunction that returns null
+        favorites.clear();
+        favorites.put("Jenny", "Bus Tour");
+        favorites.put("Tom", null);
+        BiFunction<String, String, String> mapper1 = (v1, v2) -> null;
+        favorites.merge("Madonna", "La Isla Bonnita", mapper1);
+        favorites.merge("Jenny", "New Jenny", mapper1);
+        favorites.merge("Tom", "Meow", mapper1);
+        System.out.println("Merge with a mapper that returns null: " + favorites);
     }
 }
