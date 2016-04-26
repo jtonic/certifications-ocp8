de404cb19d50099faa44757dbac8ae75bcab2131
jtonic <antonel.pazargic@gmail.com>
2016-04-26 15:35:29 +0300

    ClassCastException with generics and legacy raw collections - exemple 2.

--- src/ro/jtonic/cert/ocp8/ch3/generics/LegacyCode.java
+++ src/ro/jtonic/cert/ocp8/ch3/generics/LegacyCode.java
@@ -12,6 +12,10 @@ public class LegacyCode {
     private static final class Unicorn { }
 
     public static void main(String... args) {
+        List<Unicorn> unicorns = new ArrayList<>();
+        addUnicorn(unicorns);
+        Unicorn unicorn = unicorns.get(0);
+
         List dragons = new ArrayList();
         dragons.add(new Unicorn());
         printDragons(dragons);
@@ -19,5 +23,9 @@ public class LegacyCode {
 
     private static void printDragons(List<Dragon> dragons) {
         dragons.forEach(System.out::println);
+    }
+
+    private static void addUnicorn(List unicorns) {
+        unicorns.add(new Dragon());
     }
 }
