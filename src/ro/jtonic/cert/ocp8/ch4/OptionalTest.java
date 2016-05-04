c4ff4de8d63ca56ce684fb66b76276c791af9651
jtonic <antonel.pazargic@gmail.com>
2016-05-04 22:29:52 +0300

    Some simple examples with Optional

---
+++ src/ro/jtonic/cert/ocp8/ch4/OptionalTest.java
@@ -0,0 +1,26 @@
+package ro.jtonic.cert.ocp8.ch4;
+
+import java.util.function.*;
+import java.util.*;
+
+/**
+ * Created by antonelpazargic on 04/05/16.
+ */
+public class OptionalTest {
+
+    public static void main(String... args) {
+
+        Optional<List<String>> o1 = Optional.of(Arrays.asList("tiger", "bear", "elephant"));
+        o1.ifPresent(System.out::println);
+
+        Optional<String> empty = Optional.empty();
+        if (empty.isPresent()) {
+            System.out.println(empty.get());
+        } else {
+            System.out.println("The optional is empty");
+            empty.get();
+        }
+
+        empty.ifPresent(System.out::println);
+    }
+}
