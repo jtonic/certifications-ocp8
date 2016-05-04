47706f5f8e86ed673bf237a7c92bb86a282236fa
jtonic <antonel.pazargic@gmail.com>
2016-05-04 21:25:47 +0300

    simple examples with Function

---
+++ src/ro/jtonic/cert/ocp8/ch4/FunctionTest.java
@@ -0,0 +1,21 @@
+package ro.jtonic.cert.ocp8.ch4;
+
+import java.util.*;
+import java.util.function.*;
+
+/**
+ * Created by antonelpazargic on 04/05/16.
+ */
+public class FunctionTest {
+
+    public static void main(String... args) {
+        Function<String, String> toUpper = String::toUpperCase;
+        System.out.println(toUpper.apply("jtonic"));
+
+        Function<String, Integer> lettersCount = String::length;
+        System.out.println(lettersCount.apply("Antonel"));
+
+        System.out.println(lettersCount.andThen(i -> i % 2).apply("jtonic"));
+    }
+
+}
