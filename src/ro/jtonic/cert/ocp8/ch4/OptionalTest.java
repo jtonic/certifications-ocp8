441f37cdb3adedfb071c0d65d03fbe7e73db071a
jtonic <antonel.pazargic@gmail.com>
2016-05-05 22:03:18 +0300

    Chaining optionals.

--- src/ro/jtonic/cert/ocp8/ch4/OptionalTest.java
+++ src/ro/jtonic/cert/ocp8/ch4/OptionalTest.java
@@ -1,6 +1,5 @@
 package ro.jtonic.cert.ocp8.ch4;
 
-import java.util.function.*;
 import java.util.*;
 
 /**
@@ -18,7 +17,7 @@ public class OptionalTest {
             System.out.println(empty.get());
         } else {
             System.out.println("The optional is empty");
-            //            empty.get();
+            // empty.get();
         }
 
         empty.ifPresent(System.out::println);
@@ -30,6 +29,28 @@ public class OptionalTest {
         final Double no = o2.orElse(Double.NaN);
         System.out.println(no);
 
+        // display the number that are three digits
+        printIfThreeDigitsNumber(Optional.of(100));
+        printIfThreeDigitsNumber(Optional.of(1));
+        printIfThreeDigitsNumber(Optional.empty());
+
+        System.out.println(getNumberOfCharacters(Optional.of("jtonic")));
+        System.out.println(getNumberOfCharacters(Optional.empty()));
+
         empty.orElseThrow(() -> new RuntimeException("The optional is empty"));
+    }
+
+    private static Integer getNumberOfCharacters(Optional<String> o3) {
+        return o3.map(OptionalTest::calculator).orElse(-1);
+//        return o3.map(String::length).orElse(-1);
+    }
+
+    private static <T> void printIfThreeDigitsNumber(Optional<T> opt) {
+        System.out.println(opt.map(i -> "" + i).filter(s -> s.length() == 3).orElse("none"));
+    }
+
+    private static Integer calculator(String s) {
+        System.out.println("=====calculator======");
+        return s.length();
     }
 }
