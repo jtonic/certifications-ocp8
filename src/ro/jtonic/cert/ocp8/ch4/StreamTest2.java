70fb986d9114e9d4e336e5dba0c61a41741baf43
jtonic <antonel.pazargic@gmail.com>
2016-05-06 19:33:58 +0300

    Simple examples with Stream#reduce api.

--- src/ro/jtonic/cert/ocp8/ch4/StreamTest2.java
+++ src/ro/jtonic/cert/ocp8/ch4/StreamTest2.java
@@ -17,6 +17,14 @@ public class StreamTest2 {
         getMax(lst).ifPresent(System.out::println);
         getMin(lst).ifPresent(System.out::println);
 
+        List<String> chars = Arrays.asList("w", "o", "l", "f");
+        final String concat = chars.stream().reduce("", (a, b) -> a + b);
+        System.out.println("simple stream reduce to concatenate " + concat);
+
+        System.out.println("Simple stream reduce with no identity version api. the method returns Optional in this case because the stream can be empty");
+        chars.stream().reduce(String::concat).ifPresent(System.out::println);
+        final String conc2 = Stream.<String>empty().reduce(String::concat).orElse("n/a");
+        System.out.println("conc2 = " + conc2);
     }
 
     private static Optional<String> getMax(List<String> lst) {
