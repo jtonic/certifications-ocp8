ff8ba921a1f8e5d00a15aa427f6b9f1d6a0029b5
jtonic <antonel.pazargic@gmail.com>
2016-05-07 17:16:34 +0300

    Simple examples with further intermediate operations such as map, limit, skip, distinct.

--- src/ro/jtonic/cert/ocp8/ch4/StreamTest4.java
+++ src/ro/jtonic/cert/ocp8/ch4/StreamTest4.java
@@ -11,6 +11,15 @@ public class StreamTest4 {
     public static void main(String... args) {
         List<String> animals = Arrays.asList("monkey", "lion", "tiger");
         animals.stream().filter(a -> a.startsWith("l")).forEach(System.out::println);
+
+        animals = Arrays.asList("goose", "duck", "duck", "goose", "chicken");
+        animals.stream().distinct().forEach(System.out::print);
+
+        Stream.iterate(1, i -> i + 1).limit(5).forEach(System.out::println);
+        Stream.iterate(1, i -> i + 1).skip(5).limit(5).forEach(System.out::print);
+
+        System.out.println("======");
+        Stream.of("Gorilla", "Monkey", "Snake", "Lion", "Tiger", "Giraffe").map(String::length).forEach(System.out::print);
     }
 
 }
