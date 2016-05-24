a4a5253e228b40867675345f365629f6d82277d6
jtonic <antonel.pazargic@gmail.com>
2016-05-24 15:32:52 +0300

    Some simple examples with assertions, but the java assert related switches are configured to enable, disable assertions for packages and classes.

---
+++ src/ro/jtonic/cert/ocp8/ch6/On.java
@@ -0,0 +1,10 @@
+package ro.jtonic.cert.ocp8.ch6;
+
+/**
+ * Created by antonelpazargic on 18/05/16.
+ */
+public class On {
+    public void foo() {
+        assert false;
+    }
+}
---
+++ src/ro/jtonic/cert/ocp8/ch6/Test8.java
@@ -0,0 +1,12 @@
+package ro.jtonic.cert.ocp8.ch6;
+
+/**
+ * Created by antonelpazargic on 18/05/16.
+ */
+public class Test8 {
+
+    public static void main(String... args) {
+        new On().foo();
+        System.out.println("main");
+    }
+}
