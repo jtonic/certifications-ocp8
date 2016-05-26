50c4b28ad0e57d44907c0ba74cf7fcb99dac091c
jtonic <antonel.pazargic@gmail.com>
2016-05-26 10:29:00 +0300

    Simple example with java.util.Collections.synchronizedXXX. Pay attention that the iterator of those collections are not synchronized and the user should sync it by himself

---
+++ src/ro/jtonic/cert/ocp8/ch8/CollectionsTest.java
@@ -0,0 +1,36 @@
+package ro.jtonic.cert.ocp8.ch8;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * Created by antonelpazargic on 26/05/16.
+ */
+public class CollectionsTest {
+
+    public static void main(String... args) {
+        List<Integer> lst1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> concLst1 = Collections.synchronizedList(lst1);
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(10);
+            service.submit(() -> {
+                synchronized(concLst1) {
+                    concLst1.forEach(System.out::print);
+                }
+            }); // the iterator is not synchronized. It should be synchronized by the user
+            for(int i = 0; i < 100; i++) {
+                service.submit(() -> concLst1.add(new Random().nextInt(100)));
+            }
+            service.submit(() -> {
+                synchronized(concLst1) {
+                    concLst1.forEach(System.out::println);
+                }
+            });
+        } finally {
+            if (service != null && !service.isShutdown()) service.shutdown();
+        }
+    }
+}
+
+
