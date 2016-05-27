5f5e1c3244a203d82bbeb18c41cf3707c8b35108
jtonic <antonel.pazargic@gmail.com>
2016-05-27 22:45:31 +0300

    A simple example with a Executors.newSingleThreadScheduledExecutor and ScheduledFuture. Take note that the ScheduledFuture implements the Delayed interface that has only one method getDelay which is the remaining delay of the task (zero or negative value means the task was executed).

---
+++ src/ro/jtonic/cert/ocp8/ch8/SchedulingTasksTest.java
@@ -0,0 +1,30 @@
+package ro.jtonic.cert.ocp8.ch8;
+
+import java.util.concurrent.*;
+/**
+ * Created by antonelpazargic on 27/05/16.
+ */
+public class SchedulingTasksTest {
+
+    public static void main(String... arfs) throws java.lang.InterruptedException {
+        ScheduledExecutorService scheduledService = null;
+        try {
+            System.out.println("Begin");
+            scheduledService = Executors.newSingleThreadScheduledExecutor();
+            ScheduledFuture<?> future = scheduledService.schedule(
+                    () -> {
+                        System.out.println("Executed after a specified time interval.");
+                    }
+                    , 10, TimeUnit.SECONDS);
+            TimeUnit.SECONDS.sleep(4);
+            System.out.println("Future's delay: " + future.getDelay(TimeUnit.SECONDS));
+
+            System.out.println("End");
+        } finally {
+            if (scheduledService != null) {
+                scheduledService.shutdown();
+            }
+        }
+    }
+
+}
