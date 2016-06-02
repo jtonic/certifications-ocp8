4468246222ad68ab06f23ccbcd646cd20c4747af
jtonic <antonel.pazargic@gmail.com>
2016-06-02 11:26:05 +0300

    Simple example of copying a file using the low level java.io API FileInputStream and FileOutputStream. The performance is poor for large files, cause a numerous round trips between the java process and file system.

--- src/ro/jtonic/cert/ocp8/ch8/IOSamples.java
+++ src/ro/jtonic/cert/ocp8/ch8/IOSamples.java
@@ -7,12 +7,13 @@ import java.io.*;
 public class IOSamples {
 
     private static final String FILE_NAME = "/Users/antonelpazargic/jtonic/git/github/certifications-ocp8/src/ro/jtonic/cert/ocp8/ch8/Zoo.txt";
+    private static final String NEW_FILE_NAME = "/Users/antonelpazargic/jtonic/git/github/certifications-ocp8/src/ro/jtonic/cert/ocp8/ch8/Zoo1.txt";
 
     public static void main(String... args) throws IOException {
-
-        testMark();
-
+        copy(FILE_NAME, NEW_FILE_NAME);
         System.exit(0);
+        testSkip();
+        testMark();
 
         try(
                 BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME));
@@ -24,6 +25,18 @@ public class IOSamples {
             String line;
             while((line = br.readLine()) != null) {
                 System.out.println(line);
+            }
+        }
+    }
+
+    private static void copy(String from, String to) throws FileNotFoundException, IOException {
+        try (
+                InputStream is = new FileInputStream(from);
+                OutputStream os = new FileOutputStream(to)
+        ) {
+            int b;
+            while ((b = is.read()) != -1) {
+                os.write(b);
             }
         }
     }
@@ -40,7 +53,7 @@ public class IOSamples {
                 System.out.print((char) is.read());
                 System.out.print((char) is.read());
                 System.out.print((char) is.read());
-                is.reset();
+                is.reset();  // in this case an exception is not thrown even if the mark position has been invalidated (the mark read limit has been passed). The API specifies that an exception may be thrown.
             } else {
                 System.out.println("\nMark is not supported in the FileInputStream.\n");
                 // is.mark(4); // mark() doesn't throw a IOException
@@ -51,6 +64,23 @@ public class IOSamples {
             System.out.print((char) is.read());
             System.out.print((char) is.read());
             System.out.print((char) is.read());
+        }
+    }
+
+    private static void testSkip() throws IOException {
+        try(Reader reader = new StringReader("jtonic")) {
+            System.out.print((char) reader.read());
+            long actuallySkipped = reader.skip(4);
+            System.out.println("\nActually skipped chars: " + actuallySkipped);
+            if (actuallySkipped <= 0) {
+                System.out.println("No chars actually skipped");
+                throw new RuntimeException("No chars were actually skipped.");
+            }
+            int readChar;
+            while ((readChar = reader.read()) != -1) {
+                System.out.print((char) readChar);
+            }
+
         }
     }
 }
