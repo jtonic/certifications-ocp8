73d7b72bbc7ccee2f780d3f1c81ced225fad1888
jtonic <antonel.pazargic@gmail.com>
2016-06-02 15:01:37 +0300

    Simple examples with the java.io.PrintWriter API

---
+++ src/ro/jtonic/cert/ocp8/ch8/PrintWriterTest.java
@@ -0,0 +1,46 @@
+package ro.jtonic.cert.ocp8.ch8;
+
+import java.io.*;
+/**
+ * Created by antonelpazargic on 02/06/16.
+ */
+public class PrintWriterTest {
+
+    private static final File FILE = new File("/Users/antonelpazargic/jtonic/git/github/certifications-ocp8/src/ro/jtonic/cert/ocp8/ch8/printwriter1.log");
+
+    private static final class Person {
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [name= " + this.name + "]";
+        }
+    }
+
+    public static void main(String... args) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(System.out);
+        out.print(5);
+        // out.write(System.getProperty("line.separator"));
+        out.print(0.5);
+        out.println();
+        out.println("Antonel is the best person ever, but he is quite selfish once in a while.");
+        Person person = new Person("Antonel");
+        out.println(person);
+
+        out.write(person == null ? "null" : String.valueOf(person));
+        out.println();
+
+        System.out.println("line separator: " + System.getProperty("line.separator"));
+
+        final boolean anyErrors = out.checkError();
+        if (anyErrors) {
+            System.out.println("Errors has been encountered.");
+        } else {
+            System.out.println("No print to console errors has been encountered.");
+        }
+    }
+}
