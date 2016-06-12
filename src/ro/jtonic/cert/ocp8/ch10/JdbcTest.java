e7d759459ae6d1fd5a1f9ac1a646b55f5fd150a6
jtonic <antonel.pazargic@gmail.com>
2016-06-12 19:51:35 +0300

    An example with statement.execute

--- src/ro/jtonic/cert/ocp8/ch10/JdbcTest.java
+++ src/ro/jtonic/cert/ocp8/ch10/JdbcTest.java
@@ -4,14 +4,25 @@ import java.sql.*;
 public final class JdbcTest {
 
 	public static void main(String... args) throws SQLException {
-		String url = "jdbc:mysql://localhost/ch10";
+		String url = "jdbc:mysql://localhost:3306/ch10";
+		// Use the following to load the Mysql driver, in case the jdbc provider doesn't offer the /META-INF/services/java.sql.Driver
+		// and don't forget to declare java.lang.ClassNotFoundException
+		// Class.forName("com.mysql.jdbc.Driver");
 		try (	
 			Connection con = DriverManager.getConnection(url, "ch10", "ch10");
-			Statement stm = con.createStatement();
-			ResultSet rs = stm.executeQuery("select id, name, num_acres from species");
+			Statement stm = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
 		) {
-			while(rs.next()) {
-				System.out.println("id = " + rs.getInt(1) + ", name = " + rs.getString("name") + ", num_acres = " + rs.getDouble("num_acres"));
+			boolean isResultSet = stm.execute("select id, name, num_acres from species");
+			if (isResultSet) {	
+				ResultSet rs = stm.getResultSet();
+				System.out.println("A query was executed");
+				System.out.println("=================");
+				while (rs.next()) {
+					System.out.println(rs.getString(2));
+				}
+			} else {
+				int affectedRows = stm.getUpdateCount();
+				System.out.println("The affected rows: " + affectedRows);
 			}
 		}
 	}
