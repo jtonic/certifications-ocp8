608382b3c1f448729fc167afc47d56b8c45a4c1b
jtonic <antonel.pazargic@gmail.com>
2016-06-13 12:40:38 +0300

    some examples with TYPE_SCROLL_INSENSITIVE

--- src/ro/jtonic/cert/ocp8/ch10/JdbcTest.java
+++ src/ro/jtonic/cert/ocp8/ch10/JdbcTest.java
@@ -11,17 +11,49 @@ public final class JdbcTest {
 		// Class.forName("com.mysql.jdbc.Driver");
 		try (	
 			Connection con = DriverManager.getConnection(url, "ch10", "ch10");
-			Statement stm = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			ResultSet rs = stm.executeQuery("select name, date_born, id from animals");
 		) {
-			if (rs.next()) {
-					System.out.println(rs.getTime(2).toLocalTime());
-					System.out.println(rs.getDate(2).toLocalDate());
-					System.out.println(rs.getTimestamp(2).toLocalDateTime());
+			rs.afterLast();		
+			rs.previous();
+			print(rs);
 
-					LocalDateTime birthDate = ((Timestamp) rs.getObject("date_born")).toLocalDateTime();
-					System.out.println("birth date = " + birthDate);
-			}
+			rs.beforeFirst();
+			rs.next();
+			print(rs);			
+
+			rs.absolute(2);
+			print(rs);
+
+			rs.relative(2);
+			print(rs);
+
+			// The following throws SQLException
+			rs.absolute(0);
+			print(rs);
+
+			// there are 6 rows in the table. The following throws SQLException
+			rs.absolute(7);
+			print(rs);
+
+			// after the end of the result set
+			rs.afterLast();
+			print(rs);
+			
+			// before the firs row
+			rs.beforeFirst();
+			print(rs);
+
+			// exceeded the rows number;
+			rs.relative(3);
+			print(rs);
 		}
+	}
+
+	private static void print(ResultSet rs) throws SQLException {
+			System.out.println("Name: " + rs.getObject("name"));
+			LocalDateTime birthDate = rs.getTimestamp("date_born").toLocalDateTime();
+			System.out.println("Birth date: " + birthDate);
+			System.out.println("Id: " + (Integer)rs.getObject("id"));
 	}
 }
