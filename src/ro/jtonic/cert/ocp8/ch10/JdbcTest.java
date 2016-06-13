51da1b2f2a49ae0d8aba3994d5c5351eea89ac76
jtonic <antonel.pazargic@gmail.com>
2016-06-13 13:07:08 +0300

    a simple example with catching the SQLException. e.getMessage(), e.getSQLState(), e.getErrorCode()

--- src/ro/jtonic/cert/ocp8/ch10/JdbcTest.java
+++ src/ro/jtonic/cert/ocp8/ch10/JdbcTest.java
@@ -4,16 +4,52 @@ import java.time.*;
 
 public final class JdbcTest {
 
+	private static final String URL  = "jdbc:mysql://localhost:3306/ch10";
 	public static void main(String... args) throws SQLException {
-		String url = "jdbc:mysql://localhost:3306/ch10";
+		test2();
+
+		System.exit(0);
+
+		test1();
+	}
+
+	public static void test2() {
+		try (	
+			Connection con = DriverManager.getConnection(URL, "ch10", "ch10");
+			Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = stm.executeQuery("select name, date_born, id from animals");
+		) {
+			if(rs.first()) {
+				System.out.println("name: " + rs.getString("name1"));
+			}	
+		} catch(SQLException e) {
+			System.out.println("e.getMessage(): " + e.getMessage());
+			System.out.println("e.getSQLState(): " + e.getSQLState());
+			System.out.println("e.getErrorCode(): " + e.getErrorCode());
+		}
+	}
+
+	public static void test1() throws SQLException {
 		// Use the following to load the Mysql driver, in case the jdbc provider doesn't offer the /META-INF/services/java.sql.Driver
 		// and don't forget to declare java.lang.ClassNotFoundException
 		// Class.forName("com.mysql.jdbc.Driver");
 		try (	
-			Connection con = DriverManager.getConnection(url, "ch10", "ch10");
+			Connection con = DriverManager.getConnection(URL, "ch10", "ch10");
 			Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			ResultSet rs = stm.executeQuery("select name, date_born, id from animals");
 		) {
+
+
+			if(rs.first()) {
+				System.out.print("rs.first: ");
+				print(rs);
+			}
+			
+			if(rs.last()) {
+				System.out.print("rs.last(): ");
+				print(rs);
+			}
+
 			rs.afterLast();		
 			rs.previous();
 			print(rs);
@@ -28,13 +64,26 @@ public final class JdbcTest {
 			rs.relative(2);
 			print(rs);
 
+
+			if (rs.absolute(-1)) {
+				System.out.print("rs.absolute(-1))");
+				print(rs);
+			}
+
+			if (rs.relative(-1)) {
+				System.out.print("rs.relative(-1)");
+				print(rs);
+			}
+
 			// The following throws SQLException
-			rs.absolute(0);
-			print(rs);
+			if (rs.absolute(0)) {
+					print(rs);
+			}
 
 			// there are 6 rows in the table. The following throws SQLException
-			rs.absolute(7);
-			print(rs);
+			if (rs.absolute(7)) {
+					print(rs);
+			}
 
 			// after the end of the result set
 			rs.afterLast();
